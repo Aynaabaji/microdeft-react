941bc4a8ad1ebf3094d52a2daf6f06bd59ef9de3
Adi <skb.sharafat@gmail.com>
2023-06-03 15:52:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Form from './components/Form'
+import './components/Nav'
+import Nav from './components/Nav'
+
+function App() {
+
+  return (
+    <>
+      <Nav />
+      <Form />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div className='container'>
+      <form action="" className='form'>
+        <label htmlFor="">Product Name</label>
+        <input className='form-control' type="text" name="product" id="product" />
+        <label htmlFor="">Description</label>
+        <input className='form-control' type="text" name="product" id="product" />
+        <label htmlFor="">Image URL</label>
+        <input className='form-control' type="text" name="product" id="product" />
+        <label htmlFor="">Category</label>
+        <select className='form-control' name="" id="">
+            <option value="1">Electronics</option>
+            <option value="1">Jewelery</option>
+            <option value="1">Men's Clothingg</option>
+            <option value="1">Women's Cloathing</option>
+        </select>
+        <label htmlFor="">Product Name</label>
+        <input className='form-control' type="text" name="product" id="product" /><br />
+        <input className='form-control' type="submit" value="Add Products" />
+      </form>
+    </div>
+  )
+}
+
+export default Form
