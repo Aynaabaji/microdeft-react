6e1ea97566f40319b5290d1fff27257c9ef2614b
Adi <skb.sharafat@gmail.com>
2023-06-03 16:15:54 +0600

    first commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,12 +1,126 @@
 import React from 'react'
+import Image from '../assets/prod1.jpg'
+import './product.css'
 
 const Products = () => {
 
-    const product = []
+    const product = [
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+        {
+        pn:"Bag",
+        img:Image,
+        price:30,
+        short:"Men's Cloathing"
+        },
+    ]
 
   return (
-    <div>
-      
+    <div className='container'>
+        <div className="prod">
+        {
+            product.map((prods)=>(
+                <div className="product" key={Math.random()}>
+                    <div className="prod_image">
+                        <img src={prods.img} alt="pic" />
+                    </div>
+                    <div className="prod_details">
+                        <h3>{prods.pn}</h3>
+                        <p>{prods.price}</p>
+                        <h5>{prods.short}</h5>
+                    </div>
+                </div>
+            ))
+        }
+        </div>
     </div>
   )
 }
