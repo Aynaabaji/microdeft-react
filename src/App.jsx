1c3019ecc147130d739445b29a543edc0f4fd90b
Adi <skb.sharafat@gmail.com>
2023-06-03 16:28:11 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <>
       <Nav />
       <Form />
+      <br /><br />
       <Products />
     </>
   )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,11 +5,11 @@ const Form = () => {
     <div className='container'>
       <form action="" className='form'>
         <label htmlFor="">Product Name</label>
-        <input className='form-control' type="text" name="product" id="product" />
+        <input className='form-control' type="text" />
         <label htmlFor="">Description</label>
-        <input className='form-control' type="text" name="product" id="product" />
+        <input className='form-control' type="text" />
         <label htmlFor="">Image URL</label>
-        <input className='form-control' type="text" name="product" id="product" />
+        <input className='form-control' type="text" />
         <label htmlFor="">Category</label>
         <select className='form-control' name="" id="">
             <option value="1">Electronics</option>
@@ -18,7 +18,7 @@ const Form = () => {
             <option value="1">Women's Cloathing</option>
         </select>
         <label htmlFor="">Product Name</label>
-        <input className='form-control' type="text" name="product" id="product" /><br />
+        <input className='form-control' type="text" /><br />
         <input className='form-control' type="submit" value="Add Products" />
       </form>
     </div>
